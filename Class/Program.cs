08fa30b952033c677166c7cfad18323f2e67c85f
kazzhim <118839750+kazzhim@users.noreply.github.com>
2022-12-19 16:14:54 +0400

    Add files via upload

---
+++ Class/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Telebenin adini yazin: ");
+string name = Console.ReadLine();
+
+Console.Write("Telebenin soyadini yazin: ");
+string surname = Console.ReadLine();
+
+Console.Write("Telebenin qrup nomresini yazin: ");
+string group = Console.ReadLine();
+
+Console.Write("Telebenin qiymetini yazin: ");
+int point = int.Parse(Console.ReadLine());
+
+Console.Write("Telebe veziyyeti");
+string isGraduated = Console.ReadLine();
+
+
+Student student = new Student(name, surname, group, point, isGraduated);
+
+
+student.Informations();
+Console.WriteLine("*********************");
+student.IsValid();
+
+
+
+
+
+
+
+
+
+
+
---
+++ Class/Student.cs
@@ -0,0 +1,58 @@
+﻿public class Student 
+{  
+
+    public Student(string name, string surname, string group, int point, string isGraduated)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Group = group;
+        this.Point = point;
+
+        if (isGraduated == "Mezun")
+            this.IsGraduated = true;
+        else
+            this.IsGraduated = false;
+    }
+
+    public string Name;
+    public string Surname;
+    public string Group;
+    public int Point;
+    public bool IsGraduated;
+
+
+    public string NameAndSurname(string name, string surname)
+    {
+        return $"{name} {surname}";
+    }
+
+    internal void Informations()
+    {
+        Console.WriteLine($"Telebenin adi: {this.Name} \n" +
+                          $"Soyadi: {this.Surname} \n" +
+                          $"Qrupu : {this.Group} \n" +
+                          $"Qiymet : {this.Point} \n");
+
+        
+
+
+        Console.WriteLine("*********************");
+
+        if (this.IsGraduated = true)
+            Console.WriteLine("Bu telebe mezun olub.");
+
+        else
+            Console.WriteLine("Mezun olmayib");
+    }
+
+    public void IsValid()
+    {
+        if (this.Point >= 80)
+            Console.WriteLine("Ikinci imtahana gire biler");
+
+        else
+            Console.WriteLine("Ikinci imtahana gire bilmez");
+
+    }
+}
+
